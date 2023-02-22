073f6a368662bcb1c27048fd8c0b1245a13a8a6e
DESKTOP-B14EHGD\Dell <meharaweeratunga@gmail.com>
2023-02-22 21:10:22 +0530

    Added login and signup and remade the home page.

--- front end/src/App.js
+++ front end/src/App.js
@@ -1,44 +1,58 @@
 import logo from './logo.svg';
-import serum from './Images/serum.png';
-import hairloss from './Images/hairloss.jpg';
-import hair from './Images/hair.jpg';
+import home from './Images/home.png';
 import './App.css';
+import React, { useState } from 'react';
+import LoginRegister from './Components/LoginRegister';
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const toggleLogin = () => {
+    setShowLogin(!showLogin);
+  }
+
   return (
-    <div className="root">
-    <header className="header">
-      <img src={logo} alt="Skin Disease Detection logo" className="header-logo"/>
-      <nav className="header-nav">
-        <ul className="header-nav-list">
-          <li className="header-nav-item">
-            <a href="#" className="header-nav-link">Home</a>
-          </li>
-          <li className="header-nav-item">
-            <a href="#" className="header-nav-link">About Us</a>
-          </li>
-          <li className="header-nav-item">
-            <a href="#" className="header-nav-link">Services</a>
-          </li>
-          <li className="header-nav-item">
-            <a href="#" className="header-nav-link">Contact Us</a>
-          </li>
+    <div className={`root ${showLogin ? 'blur' : ''}`}>
+      <header className="header">
+        <img src={logo} alt="Skin Disease Detection logo" className="header-logo"/>
+        <nav className="header-nav">
+          <ul className="header-nav-list">
+            <li className="header-nav-item">
+              <a href="#" className="header-nav-link">HOME</a>
+            </li>
+            <li className="header-nav-item">
+              <a href="#" className="header-nav-link">ABOUT US</a>
+            </li>
+            <li className="header-nav-item">
+              <a href="#" className="header-nav-link">SERVICES</a>
+            </li>
+            <li className="header-nav-item">
+              <a href="#" className="header-nav-link">CONTACT US</a>
+            </li>
           </ul>
         </nav>
-        <button className="header-login-button">Login</button>
+        <button className="header-login-button" onClick={toggleLogin}>Login</button>
       </header>
       <main className="main">
-        <div className='right-cont'>
-          <h1 className='header-title'> ScalpCare </h1>
-          <h4 className='head-text2'>HAIR AND SCALP DISEASE IDENTIFIER </h4>
-          <button className="detect-button1">Get Started</button>
+  {!showLogin && (
+    <>
+      <div className='right-cont'>
+        <h1 className='header-title'> ScalpCare </h1>
+        <h4 className='head-text2'>HAIR AND SCALP DISEASE IDENTIFIER </h4>
+        <button className="right-cont.detect-button">Get Started</button>
+      </div>
+      <div className='left-cont'>
+        <img src={home} className='home-image' alt='home image'></img>
+
+      </div>
+    </>
+  )}
+</main>
+      {showLogin && (
+        <div className="login-wrapper">
+          <LoginRegister />
         </div>
-        <div className='left-cont'>
-          <img src={serum} className='serum-image'></img>
-          <img src={hairloss} className='hairloss-image'></img>
-          <img src={hair} className='hair-image'></img>
-        </div>
-      </main>
+      )}
     </div>
   );
 }
