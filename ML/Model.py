9c08e84e41df8d5ea3b07e5b88d474ecffa4303b
Kavini Geethasinghe <101565899+geethasinghekavini@users.noreply.github.com>
2023-03-05 21:15:07 +0530

    Update Model

--- ML/Model.py
+++ ML/Model.py
@@ -102,3 +102,124 @@ print('\nTrain time: %.3f'%(train_time/60), ' mins')
 
 model.save("ScalpCare-Final.h5")
 print("Model Saved as : ScalpCare-Final.h5")
+
+import json
+np.save('my_history.npy',history.history)
+import pandas as pd
+
+# convert the history.history dict to a pandas DataFrame:     
+hist_df = pd.DataFrame(history.history) 
+
+# save to json:  
+hist_json_file = 'history.json' 
+with open(hist_json_file, mode='w') as f:
+    hist_df.to_json(f)
+
+# or save to csv: 
+hist_csv_file = 'history.csv'
+with open(hist_csv_file, mode='w') as f:
+    hist_df.to_csv(f)
+    
+from time import time
+start = time()
+evl = model.evaluate(test_set)
+test_time = time() - start
+
+test_loss, test_acc = evl[0]*100, evl[1]*100
+
+msg = f'Test Accuracy = {test_acc:5.2f} %'
+msg1 = f'Test Loss = {test_loss:5.2f} %'
+msg2 = f'Test Time = {test_time:5.2f} s'
+print(msg)
+print(msg1)
+print(msg2)
+
+# Plotting Loss & Accuracy Graphs
+from sklearn.metrics import confusion_matrix
+plt.figure(figsize=(12, 12))
+plt.subplot(3, 2, 1)
+plt.plot(history.history['acc'], label = 'train_accuracy')
+plt.plot(history.history['val_acc'], label = 'val_accuracy')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.legend()
+plt.subplot(3, 2, 2)
+plt.plot(history.history['loss'], label = 'train_loss')
+plt.plot(history.history['val_loss'], label = 'val_loss')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
+
+import numpy as np
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import itertools
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(15, 15))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()        
+
+from sklearn.metrics import confusion_matrix
+import numpy as np
+
+classes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+
+y_true = np.array(test_set.labels)
+print("True : ", y_true)
+
+y_pred = model.predict(test_set)
+y_pred = np.argmax(y_pred, axis=1)
+print("Predicted : ", y_pred)
+
+conf_mat = confusion_matrix(y_true, y_pred)
+
+plot_confusion_matrix(cm           = conf_mat,
+                      normalize    = False,
+                      target_names = classes,
+                      title        = "ScalpCare Confusion Matrix")
+
+import os
+print(os.listdir())
+
+import os
+path = os.path.join(os.getcwd(), "sample_data")
+print(path)
+
